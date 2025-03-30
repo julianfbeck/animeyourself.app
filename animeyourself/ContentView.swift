16c52eac7d897a494b3cd292c7cd4092e163dd92
Julian Beck <ju-fa-beck@t-online.de>
2025-03-30 09:07:28 +0200

    Initial Commit

---
+++ animeyourself/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  animeyourself
+//
+//  Created by Julian Beck on 30.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ animeyourself/animeyourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  animeyourselfApp.swift
+//  animeyourself
+//
+//  Created by Julian Beck on 30.03.25.
+//
+
+import SwiftUI
+
+@main
+struct animeyourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
